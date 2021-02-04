9c4f7397caedbe39abd47c6897a215ea229ecb80
anproagile <anproagile@gmail.com>
2021-02-04 10:32:32 +0200

    Minor refactoring

--- src/components/SwapOrSend.vue
+++ src/components/SwapOrSend.vue
@@ -15,14 +15,8 @@
       />
 
       <FormIcon :style="'padding-top: 0.25rem; padding-bottom: 0.25rem;'">
-        <button
-          class="p-2 rounded-full transition ease-in-out duration-300 hover:bg-alphawhite focus:outline-none rotate-when-hover"
-          @click="changeDirections"
-        >
-          <img
-            src="@/assets/arrow-down.svg"
-            style="width: 17px; height: 17px;"
-          />
+        <button class="p-2 rounded-full transition ease-in-out duration-300 hover:bg-alphawhite focus:outline-none rotate-when-hover" @click="changeDirections">
+          <img src="@/assets/arrow-down.svg" style="width: 17px; height: 17px;" />
         </button>
       </FormIcon>
 
@@ -86,23 +80,11 @@
             :class="
               activeSlippagePercentage === percentage ? 'bg-alphawhite' : ''
             "
-            v-on:click="setActiveSlippagePercentage(percentage)"
+            v-on:click="activeSlippagePercentage = percentage"
           >
             {{ percentage }}
             <span class="opacity-75">%</span>
           </button>
-
-          <div
-            class="px-2 py-2 ml-2 text-xs font-light rounded-md shadow-xs focus:outline-none leading-tight"
-          >
-            <input
-              class="bg-transparent outline-none text-right w-12"
-              v-bind:placeholder="activeSlippagePercentage"
-              v-model="customSlippagePercentage"
-              @input="e => handleCustomSlippageChange(e.target.value)"
-            />
-            %
-          </div>
         </div>
 
         <div class="flex justify-between mb-1">
@@ -183,8 +165,7 @@ export default class SwapOrSend extends Vue {
   recipientAddress: string = "";
 
   slippagePercentages = [0.5, 1, 3];
-  activeSlippagePercentage: number | undefined = 1;
-  customSlippagePercentage: string = "";
+  activeSlippagePercentage = 1;
   fee: string | null = null;
   inputDexAddress: string | null = null;
   outputDexAddress: string | null = null;
@@ -219,7 +200,7 @@ export default class SwapOrSend extends Vue {
   get minimumReceived() {
     if (!this.outputToken || !this.outputAmount) return null;
     const base = new BigNumber(100)
-      .minus(this.activeSlippagePercentage || 0)
+      .minus(this.activeSlippagePercentage)
       .div(100)
       .times(this.outputAmount);
 
@@ -230,13 +211,9 @@ export default class SwapOrSend extends Vue {
     const inAndOutValid =
       this.inputToken &&
       this.outputToken &&
-      [
-        this.inputAmount,
-        this.outputAmount,
-        this.minimumReceived,
-        this.activeSlippagePercentage,
-      ].every(a => a && +a > 0) &&
-      this.activeSlippagePercentage! < 100;
+      [this.inputAmount, this.outputAmount, this.minimumReceived].every(
+        (a) => a && +a > 0
+      );
     return this.send
       ? inAndOutValid && isAddressValid(this.recipientAddress)
       : inAndOutValid;
@@ -336,11 +313,6 @@ export default class SwapOrSend extends Vue {
     this.calcOutputAmount();
   }
 
-  setActiveSlippagePercentage(percentage: number) {
-    this.activeSlippagePercentage = percentage;
-    this.customSlippagePercentage = "";
-  }
-
   handleInputAmountChange(amount: string) {
     this.inputAmount = amount;
     const isNum = /^[0-9.]*$/g.test(amount);
@@ -358,13 +330,6 @@ export default class SwapOrSend extends Vue {
       this.calcInputAmount();
     } else {
       this.inputAmount = "";
-    }
-  }
-
-  handleCustomSlippageChange(amount: string) {
-    const numAmount = amount ? Number(amount) : undefined;
-    if (numAmount !== undefined && !Number.isNaN(numAmount)) {
-      this.activeSlippagePercentage = numAmount;
     }
   }
 
@@ -402,8 +367,6 @@ export default class SwapOrSend extends Vue {
           await getDexStorage(this.outputToken.exchange),
           this.outputToken
         );
-        break;
-      default:
         break;
     }
 
@@ -444,8 +407,6 @@ export default class SwapOrSend extends Vue {
           await getDexStorage(this.inputToken.exchange),
           this.inputToken
         );
-        break;
-      default:
         break;
     }
 
